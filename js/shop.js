69f586ff3f3a9a0587cf7993734db87da025ea44
Joryn <erwi0014@algonquinlive.com>
2026-03-05 01:48:26 -0500

    creates search functionality

--- js/shop.js
+++ js/shop.js
@@ -1,6 +1,7 @@
 const itemHolder = document.getElementById('gallery');
 let itemType = "all";
 let allItemTypes = ['artifacts', 'books', 'potions', 'spells'];
+let items;
 
 const defaultButton = document.getElementById("productSelectedDefaultButton");
 
@@ -8,20 +9,34 @@ window.addEventListener("load", (event) => {
     if(localStorage.getItem("itemType")){
         itemType = localStorage.getItem("itemType");
     }
-    updateDefaultButton();
-    updateActiveButton();
-    fetchItems();
+    if(localStorage.getItem("searchResult")){
+        itemHolder.innerHTML = localStorage.getItem("searchResult");
+    }
+    else{
+        updateDefaultButton();
+        updateActiveButton();
+        fetchItems();
+    }
 });
+
 async function fetchItems(){
     const res = await fetch('/js/items.json');
     const data = await res.json();
-    if(res.ok && itemType == 'all'){
-        updateAllItems(data);
-    }else if(res.ok){
-        updateItems(data);
+    // if(res.ok && itemType == 'all'){
+    //     updateAllItems(data);
+    // }else if(res.ok){
+    //     updateItems(data);
+    // }
+    if(res.ok) items = data;
+    if(itemType == 'all'){
+        updateAllItems();
+    }else{
+        updateItems();
     }
 }
-function updateItems(data){
+function updateItems(){
+    localStorage.removeItem("searchResult");
+    data = items;
     let galleryHTML = [];
     // galleryHTML.push(`<div id="productSelection">
     //                         <a href="#" class="active">Potions</a>
@@ -41,7 +56,9 @@ function updateItems(data){
     });
     itemHolder.innerHTML = galleryHTML.join("");
 }
-function updateAllItems(data){
+function updateAllItems(){
+    localStorage.removeItem("searchResult");
+    data = items;
     let galleryHTML = [];
     allItemTypes.forEach(category=>{
         data[category].forEach(item=>{
@@ -80,4 +97,43 @@ function updateActiveButton(){
             button.classList.remove('active');
         }
     })
+}
+
+const searchBar = document.getElementById("search");
+
+// function filterItems(arr, query) {
+//   return arr.filter((el) => el.toLowerCase().includes(query.toLowerCase()));
+// }
+
+searchBar.addEventListener('submit', (event)=>{
+    event.preventDefault();
+    searchItems();
+})
+function searchItems(){
+    let search = searchBar.value;
+    // let dataFiltered = filterItems(data, search);
+    // console.log(dataFiltered);
+    // let dataFiltered = filterItems(items, search);
+    let dataFiltered = [];
+    let galleryHTML = [];
+
+    allItemTypes.forEach(itemType=>{
+        if (items[itemType]){
+            dataFiltered.push(items[itemType].find(({title})=> title.toLowerCase() === search.toLowerCase()));
+        };
+    });
+    // let testArray = ['hey', 'hi', 'wat'];
+    // let testSearch = 'hi';
+    // console.log(filterItems(testArray, testSearch));
+    // console.log(items);
+    // console.log("Search " + search);
+    // console.log(dataFiltered);
+
+    dataFiltered.forEach(item => {
+        if (item) galleryHTML.push(`<div class="gallery-item"><img src="${item.src}"><p class="img-label">${item.title}</p><p class="price">$${item.price}</p><a href="#" class="button">Add to Cart</a><a href="#" class="button inverted hidden hover-show">Details</a></div>`);
+    });
+    if (galleryHTML.length > 0){
+        itemHolder.innerHTML = galleryHTML.join("");
+        localStorage.setItem('searchResult',galleryHTML);
+    }
 }
